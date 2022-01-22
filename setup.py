41edf450a52731d4ace6b7265c29b9c55be0bf27
ульба <81091299+ulbwa@users.noreply.github.com>
2022-01-22 15:06:56 +0500

    2022.22.01.13

--- setup.py
+++ setup.py
@@ -1,20 +1,17 @@
-import os
-
 from setuptools import setup
-
-path = os.path.dirname(os.path.abspath(__file__))
 
 
 setup(
     name='MockupEngineer',
-    version='2022.22.01.6',
+    version='2022.22.01.13',
     packages=['MockupModule', 'MockupModule.templates'],
     url='https://github.com/ulbwazhine/MockupEngineer',
     license='MIT',
     author='Ulbwazhine',
     author_email='ulbwa@icloud.com',
     description='An simple library for creating beautiful screenshots.',
-    install_requires=open('{}/requirements.txt'.format(path), 'r').readlines(),
-    long_description=open('{}/readme.md'.format(path), 'r').read(),
-    long_description_content_type='text/markdown'
+    install_requires=[line.strip() for line in open("requirements.txt").readlines()],
+    long_description=open('readme.md', 'r').read(),
+    long_description_content_type='text/markdown',
+    include_package_data=True
 )
