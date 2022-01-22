ccf1f6e9913dcca4503efbdfe1a583a7845b29f2
ульба <81091299+ulbwa@users.noreply.github.com>
2022-01-22 14:07:17 +0500

    2022.22.01.6

--- readme.md
+++ readme.md
@@ -7,6 +7,12 @@
 
 <img align="center" width="350" src="https://raw.githubusercontent.com/ulbwazhine/MockupEngineer/main/MockupModule/templates/iphone12promax/example.png" alt="Apple iPhone 12 Pro Max">
 
+
+## Install
+```
+$ python -m pip install MockupEngineer
+```
+
 ## Usage
 
 In Python console:
@@ -32,4 +38,5 @@ $ python -m MockupModule
 ## Links
 * [Author](https://t.me/ulbwa)
 * [GitHub](https://github.com/ulbwazhine/MockupEngineer)
+* [PyPI](https://pypi.org/project/MockupEngineer/)
 
--- setup.py
+++ setup.py
@@ -1,16 +1,20 @@
+import os
+
 from setuptools import setup
+
+path = os.path.dirname(os.path.abspath(__file__))
 
 
 setup(
     name='MockupEngineer',
-    version='2022.22.01.4',
+    version='2022.22.01.6',
     packages=['MockupModule', 'MockupModule.templates'],
     url='https://github.com/ulbwazhine/MockupEngineer',
     license='MIT',
     author='Ulbwazhine',
     author_email='ulbwa@icloud.com',
     description='An simple library for creating beautiful screenshots.',
-    install_requires=open('./requirements.txt', 'r').readlines(),
-    long_description=open('./readme.md', 'r').read(),
+    install_requires=open('{}/requirements.txt'.format(path), 'r').readlines(),
+    long_description=open('{}/readme.md'.format(path), 'r').read(),
     long_description_content_type='text/markdown'
 )
