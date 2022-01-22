da324ab2298324678d2c4741ddddcfe43b0e6a28
ульба <81091299+ulbwa@users.noreply.github.com>
2022-01-22 13:26:39 +0500

    2022.22.01.2

---
+++ README.md
@@ -0,0 +1,4 @@
+<div align="center">
+  <h1>MockupEngineer</h1>
+  <p>An simple library for creating beautiful screenshots.</p>
+</div>
--- setup.py
+++ setup.py
@@ -1,12 +1,21 @@
 from setuptools import setup
 
+try:
+    from pypandoc import convert
+
+    read_md = lambda f: convert(f, 'rst')
+except ImportError:
+    read_md = lambda f: open(f, 'r').read()
+
 setup(
     name='MockupEngineer',
-    version='2022.22.01.1',
+    version='2022.22.01.2',
     packages=['MockupModule', 'MockupModule.templates'],
     url='https://github.com/ulbwazhine/MockupEngineer',
-    license='',
-    author='ulbwa',
+    license='MIT',
+    author='Ulbwazhine',
     author_email='ulbwa@icloud.com',
-    description='An simple library for creating beautiful screenshots.'
+    description='An simple library for creating beautiful screenshots.',
+    install_requires=open('./requirements.txt', 'r').readlines(),
+    long_description=read_md('README.md')
 )
