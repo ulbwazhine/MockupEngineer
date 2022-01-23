01281feec621ee3847c7f074419df90d8505bf2d
ульба <81091299+ulbwa@users.noreply.github.com>
2022-01-23 14:41:26 +0500

    2022.23.01.15

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='MockupEngineer',
-    version='2022.23.01.14',
+    version='2022.23.01.15',
     packages=find_packages(),
     url='https://github.com/ulbwazhine/MockupEngineer',
     license='MIT',
